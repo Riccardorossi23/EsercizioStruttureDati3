3d6293c12ab9cc8d8343b2f4b2cf537f4ef860d7
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-01-27 15:34:28 +0100

    Add project files.

---
+++ EsercizioStruttureDati3/Dati3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EsercizioStruttureDati3
+{
+    class Dati3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome del file sorgente: ");
+            string fileDati = $"{ Console.ReadLine()}";
+            Console.WriteLine("Nome del file nuovo: ");
+            string fileCopia = $"{ Console.ReadLine()}";
+            if (File.Exists(fileDati))
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(fileDati))
+                    {
+                        using (StreamWriter w = new StreamWriter(fileCopia))
+                        {
+                            string line;
+                            while ((line = r.ReadLine()) != null)
+                            {
+                                w.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+            else
+            {
+                Console.WriteLine("Il file sorgente non esiste");
+            }
+
+        }
+    }
+}
+        
